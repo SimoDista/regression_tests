df4869e5a48a916beff454df7b74c3edc3eb4c7f
Simone Di Stasi <simoneds@lagrev3.ee.ethz.ch>
2022-09-07 11:22:40 +0200

    debugged 2d and 3d test

--- idma_tests/idma_2d_basic/idma_2d_basic.c
+++ idma_tests/idma_2d_basic/idma_2d_basic.c
@@ -13,8 +13,6 @@ L1_DATA static unsigned char loc[MAX_BUFFER_SIZE*8];
 #define EXT_DATA_ADDR ((unsigned int)ext)
 #define TCDM_DATA_ADDR ((unsigned int)loc)
 
-//int test_idma(unsigned int src_addr, unsigned int dst_addr, unsigned int num_bytes,
-//              unsigned int dst_stride, unsigned int src_stride, unsigned int num_reps);
 int test_idma(unsigned int src_addr, unsigned int dst_addr, unsigned int num_bytes,
               unsigned int stride, unsigned int length, unsigned int ext2loc);
 
@@ -22,41 +20,26 @@ int main() {
 
   if (rt_cluster_id() != 0) return bench_cluster_forward(0);
 
-  int k = 1;
   int error_count = 0;
   int core_id = get_core_id();
-  
-  // if (get_core_id() == 0) {
 
-  /* for (int i = 1; i < MAX_BUFFER_SIZE; i = 2 * i) {
+  for (int i = 64; i < 2049; i = 2 * i) {
+    for(int j = 8; j < 33; j = 2 * j) {
       error_count += test_idma(EXT_DATA_ADDR + (core_id*MAX_BUFFER_SIZE),
-                               TCDM_DATA_ADDR + (core_id*MAX_BUFFER_SIZE), i);
+                               TCDM_DATA_ADDR + (core_id*MAX_BUFFER_SIZE), i, 64, j, 1);
     }
-    for (int i = 1; i < MAX_BUFFER_SIZE; i = 2 * i) {
-      error_count += test_idma(TCDM_DATA_ADDR + (core_id*MAX_BUFFER_SIZE),
-                               EXT_DATA_ADDR + (core_id*MAX_BUFFER_SIZE), i);
-    }*/
-  // }
-
-   for (int i = 1; i < MAX_BUFFER_SIZE; i = 2 * i) {
-     for(int j = i; j < MAX_BUFFER_SIZE; j = 2 * j) {
-         error_count += test_idma(EXT_DATA_ADDR + (core_id*MAX_BUFFER_SIZE),
-                                  TCDM_DATA_ADDR + (core_id*MAX_BUFFER_SIZE), i, j, k, 1);
-     }
-   }
+  }  
   
-   
-   for (int i = 1; i < MAX_BUFFER_SIZE; i = 2 * i) {
-     for(int j = i; j < MAX_BUFFER_SIZE; j = 2 * j) {
-         error_count += test_idma(TCDM_DATA_ADDR + (core_id*MAX_BUFFER_SIZE),
-                                  EXT_DATA_ADDR + (core_id*MAX_BUFFER_SIZE), i, j, k, 1);
-     }
-   }
+      // for(int j = 1; j < 3; j = 2 * j) {
+      //for (int i = 1; i < MAX_BUFFER_SIZE; i = 2 * i) {
+      //      error_count += test_idma(TCDM_DATA_ADDR + (core_id*MAX_BUFFER_SIZE),
+      //                       EXT_DATA_ADDR + (core_id*MAX_BUFFER_SIZE), 1, 16, 1, 0);
+      // }
+      // }
   return error_count;
-}
+  
+  }
 
-//int test_idma(unsigned int src_addr, unsigned int dst_addr, unsigned int num_bytes,
-//              unsigned int dst_stride, unsigned int src_stride, unsigned int num_reps){
 int test_idma(unsigned int src_addr, unsigned int dst_addr, unsigned int num_bytes,
               unsigned int stride, unsigned int length, unsigned int ext2loc){
 
@@ -65,75 +48,76 @@ int test_idma(unsigned int src_addr, unsigned int dst_addr, unsigned int num_byt
         //  dst_addr);
 #endif
 
-  /*for (int i = 0; i < num_bytes; i++) {
-    *(unsigned char*)(src_addr + i) = i & 0xFF;
-  }
-
-  for (int i = 0; i < num_bytes + 16; i++) {
-    *(unsigned char*)(dst_addr + i) = 0;
-    }*/
-
-  for (int i = 0;  i < length/num_bytes; i++){
-    for (int j = 0; j < num_bytes; j++){
+  float time = 0;
+  unsigned int reps = num_bytes/length;
+  unsigned int i,j;
+  
+  for (i = 0;  i < reps; i++){
+    for (j = 0; j < length; j++){
       *(unsigned char*)(src_addr + stride*i + j) = (stride*i + j) & 0xFF;
     }
   }
-    
-        
-  for (int i = 0; i < length/(num_bytes+16); i++){
-    for (int j = 0; j < num_bytes + 16; j++){
+  
+  for (i = 0; i < reps; i++){
+    for (j = 0; j < length; j++){
       *(unsigned char*)(dst_addr + stride*i + j) = 0;
-      }
     }
-
+  }
+  
     
   synch_barrier();
 
-  unsigned int dma_tx_id = plp_dma_memcpy_2d(dst_addr, src_addr, num_bytes, stride, length, 1);
+  reset_timer();
+  
+  start_timer();
+  
+  unsigned int dma_tx_id = plp_dma_memcpy_2d(src_addr, dst_addr, num_bytes, stride, length, ext2loc);
 
-  // printf("After launch, dma_id: %x, %x, %x\n", dma_tx_id, (unsigned int
-  // *)(dma_ptr + CLUSTER_DMA_FRONTEND_DONE_REG_OFFSET), (unsigned int
-  // *)(dma_ptr + CLUSTER_DMA_FRONTEND_DONE_REG_OFFSET + ((dma_tx_id &
-  // 0xf0000000) >> 26)));
-
-  // while((dma_tx_id & 0x0fffffff) > *(unsigned int *)(dma_ptr +
-  // CLUSTER_DMA_FRONTEND_DONE_REG_OFFSET + ((dma_tx_id & 0xf0000000) >> 26))) {
-  //   asm volatile("nop");
-  // }
   plp_dma_wait(dma_tx_id);
 
+  time = get_time();
+
+  stop_timer();
+    
   unsigned int test, read;
   unsigned int error = 0;
 
-  for (int i = 0; i < length/num_bytes; i++) {
-    for(int j = 0; j < num_bytes; j++){
+  for (i = 0; i < reps; i++) {
+    for(j = 0; j < length; j++){
       test = (stride*i + j) & 0xFF;
       read = *(unsigned char*)(dst_addr + stride*i + j);
-      if (test != read) {
-        printf("Error!!! Read: %x, Test:%x, Index: %d, %d \n ", read, test, i, j);
-        error++;
+      if(get_core_id() == 0){
+        //printf("test = %p, read = %p, i = %d, j = %d\n", test, read, i, j);
+        if (test != read) {
+          printf("Error!!! Read: %x, Test:%x, Index: %d, %d \n ", read, test, i, j);
+          error++;
+        } else {
+          //        printf("OK!!! Read: %x, Test:%x, Index: %d, %d \n ", read, test, i, j);
+        }
       }
     }
   }
-
-  for (int i = num_bytes; i < length/(num_bytes+16); i++) {
-   for(int j = num_bytes; j < num_bytes + 16; j++){
- 
-    test = 0;
-    read = *(unsigned char*)(dst_addr + stride*i + j);
-
-    if (test != read) {
-      printf("Error!!! Read: %x, Test:%x, Index: %d, %d \n ", read, test, i, j);
-      error++;
+  
+  
+  /*for (int i = num_bytes/length; i < (num_bytes/length)+16; i++) {
+    for(int j = 0; j < length; j++){
+      test = 0;
+      read = *(unsigned char*)(dst_addr + stride*i + j);
+      if (test != read) {
+        printf("Error2!!! Read: %x, Test:%x, Index: %d, %d \n ", read, test, i, j);
+        error++;
+      }
     }
-  }
-}
+    }*/
   
   if (get_core_id() == 0) {
-    if (error == 0)
+    if (error == 0){
       printf("OOOOOOK!!!!!!\n");
-    else
+      printf("TIME: %f, NUM_BYTES: %d, STRIDE: %d, LENGTH: %d\n", time, num_bytes, stride,  length);
+    }else{
       printf("NOT OK!!!!!\n");
+      printf("TIME: %f, NUM_BYTES: %d, STRIDE: %d, LENGTH: %d\n", time, num_bytes, stride,  length);
+    }
   }
 
   return error;
--- idma_tests/idma_3d_basic/idma_3d_basic.c
+++ idma_tests/idma_3d_basic/idma_3d_basic.c
@@ -5,7 +5,7 @@
 #include <hal/dma/idma_v1.h>
 
 #define VERBOSE
-#define MAX_BUFFER_SIZE 0x400
+#define MAX_BUFFER_SIZE 0x1000
 
 L2_DATA static unsigned char ext[MAX_BUFFER_SIZE*8];
 L1_DATA static unsigned char loc[MAX_BUFFER_SIZE*8];
@@ -13,115 +13,151 @@ L1_DATA static unsigned char loc[MAX_BUFFER_SIZE*8];
 #define EXT_DATA_ADDR ((unsigned int)ext)
 #define TCDM_DATA_ADDR ((unsigned int)loc)
 
+#define DMA_3D_SUPPORT
+
 int test_idma(unsigned int src_addr, unsigned int dst_addr, unsigned int num_bytes,
-              unsigned int *stride, unsigned int length, unsigned int ext2loc);
+              unsigned int *stride, unsigned int length, unsigned int *num_reps, unsigned int ext2loc);
 
 int main() {
 
   if (rt_cluster_id() != 0) return bench_cluster_forward(0);
 
-  int len = 1;
-  int str[2] = {0,0};
   int error_count = 0;
   int core_id = get_core_id();
+
+  int len = 8;
+  int str[2] = {8, 1024};
+  int reps[2] = {0, 1};
+  int bytes = 0;
+
+  //printf("%d\n",core_id);
+
+  //if(core_id==0) {
+  //for (int i = 4; i < 32 + 1; i = 2 * i) {
+    // for (int j = 1; j < 33; j = 2 * j) {
+      reps[0] = 256;
+      bytes = len*(reps[0]*reps[1]);
+      //    printf(" src : %x , dst %x \n", EXT_DATA_ADDR, TCDM_DATA_ADDR);
+      error_count += test_idma(EXT_DATA_ADDR + (core_id*MAX_BUFFER_SIZE),
+                               TCDM_DATA_ADDR + (core_id*MAX_BUFFER_SIZE), bytes, str, len, reps, 1);
+      //}
+    //}
+    //}
   
-  for (int i = 1; i < MAX_BUFFER_SIZE; i = 2 * i) {
-    for (str[0] = i; str[0] < MAX_BUFFER_SIZE; str[0] = 2 * str[0]) {
-      for(str[1] = str[0]; str[1] < MAX_BUFFER_SIZE; str[1] = 2 * str[1]) {
-        error_count += test_idma(EXT_DATA_ADDR + (core_id*MAX_BUFFER_SIZE),
-                                 TCDM_DATA_ADDR + (core_id*MAX_BUFFER_SIZE), i, str, len, 1);
-      }
-    }
-  }
-  
-   
-  for (int i = 1; i < MAX_BUFFER_SIZE; i = 2 * i) {
-     for (str[0] = i; str[0] < MAX_BUFFER_SIZE; str[0] = 2 * str[0]) {
-       for(str[1] = str[0]; str[1] < MAX_BUFFER_SIZE; str[1] = 2 * str[1]) {
-         error_count += test_idma(TCDM_DATA_ADDR + (core_id*MAX_BUFFER_SIZE),
-                                  EXT_DATA_ADDR + (core_id*MAX_BUFFER_SIZE), i, str, len, 1);
-       }
-     }
-   }
   return error_count;
 }
 
 int test_idma(unsigned int src_addr, unsigned int dst_addr, unsigned int num_bytes,
-              unsigned int *stride, unsigned int length, unsigned int ext2loc){
-
+              unsigned int *stride, unsigned int length, unsigned int *num_reps, unsigned int ext2loc){
+  
 #ifdef VERBOSE
   // printf("STARTING TEST FOR %d TRANSFERS FROM %p TO %p\n", num_bytes, src_addr,
         //  dst_addr);
 #endif
 
-  for (int i = 0;  i < length/num_bytes; i++){
-    for (int j = 0; j < length/num_bytes; j++){
-      for (int k = 0; k < num_bytes; k++){
-        *(unsigned char*)(src_addr + stride[0]*i + stride[1]*j + k) =
-                                    (stride[0]*i + stride[1]*j + k) & 0xFF;
+  float time = 0;
+  unsigned int i,j,k;
+
+  /* if(get_core_id() == 0){
+    printf("stride0 = %d, stride1 = %d, num_reps0 = %d, num_reps1 = %d, length = %d, num_bytes = %d\n",
+           stride[0], stride[1], num_reps[0], num_reps[1], length, num_bytes);
+           }*/
+  
+  for (i = 0;  i < num_reps[1]; i++){
+    for (j = 0; j < num_reps[0]; j++){
+      for (k = 0; k < length; k++){
+        *(unsigned char*)(src_addr + stride[1]*i + stride[0]*j + k) =
+                                    (stride[1]*i + stride[0]*j + k) & 0xFF;
       }
     }
   }
-        
-  for (int i = 0; i < length/(num_bytes+16); i++){
-    for (int j = 0; j < length/(num_bytes+16); j++){
-      for (int k = 0; k < num_bytes + 16; k++){
-        *(unsigned char*)(dst_addr + stride[0]*i + stride[1]*j + k) = 0;
+
+  /*if(get_core_id() == 0){
+  for (i = 0;  i < num_reps[1]; i++){
+    for (j = 0; j < num_reps[0]; j++){
+      for (k = 0; k < length; k++){
+        printf(" *(src_addr + %d*%d + %d*%d + %d) = %p\n",
+               stride[1], i, stride[0], j, k, *(unsigned char*)(src_addr + stride[1]*i + stride[0]*j + k));
+      }
+    }
+  }
+  }*/
+  
+  for (i = 0; i < num_reps[1]; i++){
+    for (j = 0; j < num_reps[0]; j++){
+      for (k = 0; k < length; k++){
+        *(unsigned char*)(dst_addr + stride[1]*i + stride[0]*j + k) = 0;
       }
     }
   }
     
   synch_barrier();
 
-  unsigned int dma_tx_id = plp_dma_memcpy_3d(dst_addr, src_addr, num_bytes, stride, length, 1);
-
-  // printf("After launch, dma_id: %x, %x, %x\n", dma_tx_id, (unsigned int
-  // *)(dma_ptr + CLUSTER_DMA_FRONTEND_DONE_REG_OFFSET), (unsigned int
-  // *)(dma_ptr + CLUSTER_DMA_FRONTEND_DONE_REG_OFFSET + ((dma_tx_id &
-  // 0xf0000000) >> 26)));
-
-  // while((dma_tx_id & 0x0fffffff) > *(unsigned int *)(dma_ptr +
-  // CLUSTER_DMA_FRONTEND_DONE_REG_OFFSET + ((dma_tx_id & 0xf0000000) >> 26))) {
-  //   asm volatile("nop");
-  // }
+  reset_timer();
+  
+  start_timer();
+  
+  #ifdef DMA_3D_SUPPORT
+  unsigned int dma_tx_id = plp_dma_memcpy_3d(src_addr, dst_addr, num_bytes, stride, length, num_reps, 1);
   plp_dma_wait(dma_tx_id);
+  #else
+  for(i = 0; i < num_reps[1]; i++){
+    unsigned int dma_tx_id = plp_dma_memcpy_2d(src_addr+stride[1]*i, dst_addr+stride[1]*i,
+                                               num_bytes/num_reps[1], stride[0], length, 1);
+    plp_dma_wait(dma_tx_id);
+  }
+  #endif
+
+  time = get_time();
+  
+  stop_timer();
+  
+  //  plp_dma_wait(dma_tx_id);
 
   unsigned int test, read;
   unsigned int error = 0;
 
-  for (int i = 0; i < length/num_bytes; i++) {
-    for(int j = 0; j < length/num_bytes; j++){
-      for (int k = 0; k < num_bytes; k++){
-        test = (stride[0]*i + stride[1]*j + k) & 0xFF;
-        read = *(unsigned char*)(dst_addr + stride[0]*i + stride[1]*j + k);
-        if (test != read) {
-          printf("Error!!! Read: %x, Test:%x, Index: %d, %d, %d\n ", read, test, i, j, k);
-          error++;
+  for (i = 0; i < num_reps[1]; i++) {
+    for(j = 0; j < num_reps[0]; j++){
+      for (k = 0; k < length; k++){
+        test = (stride[1]*i + stride[0]*j + k) & 0xFF;
+        read = *(unsigned char*)(dst_addr + stride[1]*i + stride[0]*j + k);
+        if(get_core_id() == 0){
+          //printf("test = %p, read = %p, i = %d, j = %d, k = %d\n", test, read, i, j, k);
+          if (test != read) {
+            //  printf("Error!!! Read: %x, Test:%x, Index: %d, %d, %d\n ", read, test, i, j, k);
+            error++;
+          } else {
+            //printf("OK!!! Read: %x, Test:%x, Index: %d, %d, %d \n ", read, test, i, j, k);
+          }
         }
       }
     }
   }
 
-  for (int i = num_bytes; i < length/(num_bytes+16); i++) {
-    for(int j = num_bytes; j < length/(num_bytes+16); j++){
-      for (int k = 0; k < num_bytes + 16; k++){
-        test = 0;
-        read = *(unsigned char*)(stride[0]*i + stride[1]*j + k);
-        
-        if (test != read) {
-          printf("Error!!! Read: %x, Test:%x, Index: %d, %d, %d \n ", read, test, i, j, k);
-          error++;
-        }
-      }
+  /*
+  if (get_core_id() == 0) {
+    for(i=0; i<500; i++){
+      test = i & 0xFF;
+      read = *(unsigned char*)(dst_addr + i);
+      printf("test = %p, read = %p, i = %d\n", test, read, i);
     }
-  }
+  }*/
   
   if (get_core_id() == 0) {
-    if (error == 0)
+    if (error == 0){
       printf("OOOOOOK!!!!!!\n");
-    else
+      printf("TIME: %f, NUM_BYTES: %d, STRIDE[0]: %d, stride[1]: %d, NUM_REPS[0]: %d, NUM_REPS[1]: %d, LENGTH: %d\n",
+             time, num_bytes, stride[0], stride[1],  num_reps[0], num_reps[1], length);
+    }
+    else {
       printf("NOT OK!!!!!\n");
+      printf("TIME: %f, NUM_BYTES: %d, STRIDE[0]: %d, stride[1]: %d, NUM_REPS[0]: %d, NUM_REPS[1]: %d, LENGTH: %d\n",
+             time, num_bytes, stride[0], stride[1],  num_reps[0], num_reps[1], length);
+    }
   }
 
+  // printf("TIME: %f, SIZE: %d\n", time, length);
+  
   return error;
 }
